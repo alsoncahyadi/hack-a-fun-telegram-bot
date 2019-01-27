56738e8b6365844add95df9d9bc43a1254a51006
alsoncahyadi <alson.cahyadi@gmail.com>
2019-01-27 22:25:41 +0700

    First Commit

---
+++ apple/wsgi.py
@@ -0,0 +1,17 @@
+"""
+WSGI config for apple project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
+"""
+
+import os
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tomdeldridge.settings")
+
+from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
+
+application = Cling(get_wsgi_application())
---
+++ tele/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class TeleConfig(AppConfig):
+    name = 'tele'
