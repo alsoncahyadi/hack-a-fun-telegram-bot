aa2f49aa990a3238a65566f73378d6895809fa86
alsoncahyadi <alson.cahyadi@gmail.com>
2019-02-06 17:46:32 +0700

    Added reply mechanism

--- tele/views.py
+++ tele/views.py
@@ -1,16 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import requests
 import logging
 import json
+import os
 
 logger = logging.getLogger(__name__)
 
+TOKEN = os.environ['TELE_TOKEN']
+TELEGRAM_REQUEST_URL = "https://api.telegram.org/bot{token}/sendMessage?chat_id={chat_id}&text={text}"
+
 # Create your views here.
 def chat(request):
-    if request.method == "POST":
-        params = json.loads(request.body)
-        print(params)
-        return HttpResponse("HI!")
+    if request.method == "POST" and request.body:
+        req_json = json.loads(request.body)
+        print(req_json)
+        
+        message = req_json['message']
+        chat_id = message['chat']['id']
+        return HttpResponse(
+            requests.get(TELEGRAM_REQUEST_URL.format(
+                token=TOKEN,
+                chat_id=chat_id,
+                text="Hello, {} {}!".format(message['from'].get('first_name', ''), message['from'].get('last_name', ''))
+            )).content
+        )
     else:
         return HttpResponse("Sneaky you!")
 
