70be7cf0a6b8a170de889064cdc9fe00331c23f4
alsoncahyadi <alson.cahyadi@gmail.com>
2019-02-06 17:13:31 +0700

    Added "logging"

--- tele/views.py
+++ tele/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import logging
+import json
+
+logger = logging.getLogger(__name__)
 
 # Create your views here.
 def chat(request):
-    return HttpResponse("HI!")
+    if request.method == "POST":
+        params = json.loads(request.body)
+        print(params)
+        return HttpResponse("HI!")
+    else:
+        return HttpResponse("Sneaky you!")
 
 def healthz(request):
     return HttpResponse("OK")
