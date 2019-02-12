b63fb5aac4c2e9379408a0d0222747cc39d4a446
alsoncahyadi <alson.cahyadi@gmail.com>
2019-02-12 23:37:46 +0700

    removed socialaccount

--- apple/settings.py
+++ apple/settings.py
@@ -43,7 +43,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'allauth',
-    'allauth.socialaccount',
     'rest_framework',
     'rest_framework.authtoken',
     'rest_auth',
